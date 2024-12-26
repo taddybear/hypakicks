bdc932c027e9a36d856a6d350250b42dc5930395
Muhammad Umar <umar.sheikh.muhammad@gmail.com>
2024-12-26 13:39:59 +0500

    added console log to show products

--- storefront/src/lib/data/products.ts
+++ storefront/src/lib/data/products.ts
@@ -86,7 +86,7 @@ export const listProducts = async ({
     )
     .then(({ products, count }) => {
       const nextPage = count > offset + limit ? pageParam + 1 : null
-      console.log("List products 82")
+      console.log("List products 82", products)
       return {
         response: {
           products,
