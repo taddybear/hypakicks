bbe2ec189f6e0347678e881a6f4b46d31609d725
Muhammad Umar <umar.sheikh.muhammad@gmail.com>
2025-01-14 10:48:14 +0500

    added decorative debugging messages for shopify order subscriber

--- backend/src/subscribers/order-placed-klaviyo.ts
+++ backend/src/subscribers/order-placed-klaviyo.ts
@@ -1,5 +1,5 @@
 import { SubscriberArgs, type SubscriberConfig } from "@medusajs/framework";
-import shopifyOrderPlacedWorkflow from "src/workflows/shopify-order-placed";
+// import shopifyOrderPlacedWorkflow from "src/workflows/shopify-order-placed";
 import { Modules } from "@medusajs/framework/utils";
 
 export default async function orderPlacedKlaviyoHandler({
@@ -7,31 +7,14 @@ export default async function orderPlacedKlaviyoHandler({
   container,
 }: SubscriberArgs<{ id: string }>) {
   console.log("Klaviyo order placed handler");
-  const orderModuleService = container.resolve(Modules.ORDER);
-  const order = await orderModuleService.retrieveOrder(data.id);
+  // const orderModuleService = container.resolve(Modules.ORDER);
+  // const order = await orderModuleService.retrieveOrder(data.id, {
+  //   relations: [],
+  // });
   // console.log("ORDER PLACED HANDLER");
   const logger = container.resolve("logger");
 
-  console.log("Order:", order);
-
-  // try {
-  //   const client = createAdminApiClient({
-  //     storeDomain: process.env.SHOPIFY_STORE_DOMAIN,
-  //     apiVersion: "2024-10",
-  //     accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
-  //   });
-  //   return client;
-  // } catch (err) {
-  //   throw err;
-  // }
-
-  // logger.info("Running Shopify Workflow bump");
-
-  // await shopifyOrderPlacedWorkflow(container).run({
-  //   input: {
-  //     orderId: data.id,
-  //   },
-  // });
+  // console.log("Order:", order);
 }
 
 export const config: SubscriberConfig = {
--- backend/src/subscribers/order-placed-shopify.ts
+++ backend/src/subscribers/order-placed-shopify.ts
@@ -1,5 +1,5 @@
 import { SubscriberArgs, type SubscriberConfig } from "@medusajs/framework";
-import shopifyOrderPlacedWorkflow from "src/workflows/shopify-order-placed";
+// import shopifyOrderPlacedWorkflow from "src/workflows/shopify-order-placed";
 import { Modules } from "@medusajs/framework/utils";
 
 export default async function orderPlacedShopifyHandler({
@@ -8,11 +8,26 @@ export default async function orderPlacedShopifyHandler({
 }: SubscriberArgs<{ id: string }>) {
   console.log("Shopify order placed handler");
   const orderModuleService = container.resolve(Modules.ORDER);
-  const order = await orderModuleService.retrieveOrder(data.id);
+  const order = await orderModuleService.retrieveOrder(data.id, {
+    relations: [],
+  });
   // console.log("ORDER PLACED HANDLER");
   const logger = container.resolve("logger");
 
   console.log("Order:", order);
+
+  if (order) {
+    const shopifyStoreDomain = process.env.SHOPIFY_STORE_DOMAIN;
+    const shopifyAccessToken = process.env.SHOPIFY_ACCESS_TOKEN;
+
+    if (shopifyStoreDomain && shopifyAccessToken) {
+      // implement the Shopify authentication and logic
+    }
+    logger.info(
+      "Shopify store domain or access token in undefined. Please set it inside the environment variables."
+    );
+  }
+  logger.info("Order could not be retrieved using the order id in subscriber");
 
   // try {
   //   const client = createAdminApiClient({
@@ -24,14 +39,6 @@ export default async function orderPlacedShopifyHandler({
   // } catch (err) {
   //   throw err;
   // }
-
-  // logger.info("Running Shopify Workflow bump");
-
-  // await shopifyOrderPlacedWorkflow(container).run({
-  //   input: {
-  //     orderId: data.id,
-  //   },
-  // });
 }
 
 export const config: SubscriberConfig = {
